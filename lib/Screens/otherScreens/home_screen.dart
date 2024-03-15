eff200a6dc60c7a8c3c4c232c0f23399f1075d59
Kennedy Mutugi Julius <kennedymutugi111@gmail.com>
2024-03-15 08:47:19 +0300

    added stripes of code

--- lib/Screens/otherScreens/home_screen.dart
+++ lib/Screens/otherScreens/home_screen.dart
@@ -1,19 +1,69 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import 'package:virtual_assistance_2/utils/colors.dart';
 import 'package:virtual_assistance_2/widgets/feature_box.dart';
 
-class HomeSreeen extends StatelessWidget {
-  const HomeSreeen({super.key});
+class HomeSreeen extends StatefulWidget {
+  const HomeSreeen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeSreeen> createState() => _HomeSreeenState();
+}
+
+class _HomeSreeenState extends State<HomeSreeen> {
+  late final SpeechToText speechToText;
+  late String lastWords;
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    speechToText = SpeechToText();
+    await speechToText.initialize();
+  }
+
+  /// Each time to start a speech recognition session
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    speechToText.stop();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Allen ChatBot"),
+        title: const Text("Allen the ChatBot"),
         centerTitle: true,
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -24,27 +74,31 @@ class HomeSreeen extends StatelessWidget {
                   child: Container(
                     height: 120,
                     width: 120,
-                    margin: EdgeInsets.only(top: 4),
+                    margin: const EdgeInsets.only(top: 4),
                     decoration: BoxDecoration(
-                        color: Pallete.assistantCircleColor,
-                        shape: BoxShape.circle),
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
                 Container(
                   height: 123,
                   decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: AssetImage("assets/virtualAssistant.png"))),
+                    shape: BoxShape.circle,
+                    image: const DecorationImage(
+                      image: AssetImage("assets/virtualAssistant.png"),
+                    ),
+                  ),
                 ),
               ],
             ),
-        
+
             //chat bubble
             Container(
               height: 120,
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              margin: EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
               decoration: BoxDecoration(
                 border: Border.all(
                   color: Pallete.borderColor,
@@ -54,41 +108,44 @@ class HomeSreeen extends StatelessWidget {
               ),
               child: Text(
                 "Good Afternoon, How can I help you with your Mental Health Today ?.",
-                style: TextStyle(
+                style: const TextStyle(
                   color: Pallete.mainFontColor,
                   fontSize: 25,
                 ),
               ),
             ),
-        
+
             //suggestion list
             FeatureBox(
-                color: Pallete.firstSuggestionBoxColor,
-                headerText: "You are Stronger than You Think",
-                descriptionText:
-                    "This quote reminds you that you possess inner strength and resilience to overcome challenges."),
-        
-            SizedBox(
+              color: Pallete.firstSuggestionBoxColor,
+              headerText: "You are Stronger than You Think",
+              descriptionText:
+                  "This quote reminds you that you possess inner strength and resilience to overcome challenges.",
+            ),
+
+            const SizedBox(
               height: 10,
             ),
-        
+
             FeatureBox(
-                color: Pallete.secondSuggestionBoxColor,
-                headerText: "Embrace Progress, Not Perfection",
-                descriptionText:
-                    "Focus on making small steps forward rather than aiming for perfection, as progress is what matters most."),
-        
-            SizedBox(
+              color: Pallete.secondSuggestionBoxColor,
+              headerText: "Embrace Progress, Not Perfection",
+              descriptionText:
+                  "Focus on making small steps forward rather than aiming for perfection, as progress is what matters most.",
+            ),
+
+            const SizedBox(
               height: 10,
             ),
-        
+
             FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor,
-                headerText: "Your Mental Health Matters",
-                descriptionText:
-                    "Take time to prioritize your mental well-being, as it is essential for overall health and happiness."),
-        
-            SizedBox(
+              color: Pallete.thirdSuggestionBoxColor,
+              headerText: "Your Mental Health Matters",
+              descriptionText:
+                  "Take time to prioritize your mental well-being, as it is essential for overall health and happiness.",
+            ),
+
+            const SizedBox(
               height: 10,
             ),
           ],
@@ -96,11 +153,16 @@ class HomeSreeen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: 
-      () {
-        
-      },
-      child: Icon(Icons.mic),
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        child: const Icon(Icons.mic),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Virtual Assistance',
       theme: ThemeData.light(
         useMaterial3: true).copyWith(
